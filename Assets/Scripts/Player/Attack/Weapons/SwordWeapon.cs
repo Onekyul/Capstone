6f4444cfce122c4a00d724b417d3651f1f307513
Onekyul <resu2002@naver.com>
2025-09-24 19:29:36 +0900

    폴더구조정리

--- Assets/Scripts/Player/Attack/Weapons/SwordWeapon.cs
+++ Assets/Scripts/Player/Attack/Weapons/SwordWeapon.cs
@@ -39,7 +39,7 @@ public class SwordWeapon : WeaponBase
         {
             if (enemy != null)
             {
-                enemy.GetComponent<EnemyController>()?.TakeDamage(damage);
+                //enemy.GetComponent<MonsterController>()?.TakeDamage(damage);
             }
         }
         
