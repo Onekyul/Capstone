aba70275d569822a3b9df4c3fd3924c7ad71d7f0
parkjt430 <66296683+parkjt430@users.noreply.github.com>
2025-12-29 03:58:49 +0900

    UI매니저가 삭제되지 않는 거에서 삭제 되는 걸로 변경

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -42,7 +42,6 @@ public class UIManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
